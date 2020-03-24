00daee961c01d54342aea65d3294564449d95d74
Aswin <aswin.harikumar@yahoo.com>
2020-03-24 23:58:31 +0530

    Improve products responses

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,41 +1,64 @@
 const express = require('express');
 const router = express.Router();
-const Product =  require('./models/products');
+const Product = require('./models/products');
 const mongoose = require('mongoose');
 router.get('/', (req, res, next) => {
-Product.find().exec().then(doc => {
-    res.status(200).json(doc);
-}).catch(err => {
-    res.status(500).json({error:err})
-});
+    Product.find().select('name price _id').exec().then(doc => {
+        const response = {
+            count: doc.length,
+            products: doc.map(el => {
+                return {
+                    name: el.name,
+                    price: el.price,
+                    request: {
+                        type: 'GET',
+                        url: req.originalUrl + el._id
+                    }
+                }})
+        };
+        res.status(200).json(response);
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    });
 });
 router.post('/', (req, res, next) => {
-     const product = new Product({
+    const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-        });
-        product.save().then(res => {
-            console.log(res)
+    });
+    product.save().then(result => {
+        res.status(201).json({
+            message: 'Created Successfully',
+            product: {
+                name: result.name,
+                price: result.price,
+                request: {
+                    type: 'GET',
+                    url: req.originalUrl + result._id
+                }
+            }
         })
-        .catch(err => {
-            console.log(err);
-        });
-    res.status(201).json({
-        message: 'POST Request maping to products',
-        createdProduct: product
+
     })
+        .catch(err => {
+            res.status(500).json({error: err})
+        });
 });
 router.get('/:id', (req, res, next) => {
     const id = req.params.id;
     Product.findById(id).exec().then((doc) => {
-        doc ? res.status(200).json(doc):res.status(404).json({error: 'Product not found'});
-    })
-    .catch((err) => {
-        res.status(500).json({
-            error: err
+            doc ? res.status(200).json(doc) : res.status(404).json({
+                error: 'Product not found'
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err
+            });
         });
-    });
 
 });
 router.patch('/:id', (req, res, next) => {
@@ -44,16 +67,26 @@ router.patch('/:id', (req, res, next) => {
     for (const ops of req.body) {
         updateObj[ops.propName] = ops.value
     }
-    Product.update({_id: id}, {$set: updateObj}).exec().then(result => {
+    Product.update({
+        _id: id
+    }, {
+        $set: updateObj
+    }).exec().then(result => {
         res.status(200).json(result);
-    }).catch(err => res.status(500).json({error:err}));
+    }).catch(err => res.status(500).json({
+        error: err
+    }));
 });
 router.delete('/:id', (req, res, next) => {
     const id = req.params.id;
-    Product.remove({_id: id}).exec().then(result => {
+    Product.remove({
+        _id: id
+    }).exec().then(result => {
         res.status(200).json(result);
     }).catch(err => {
-        res.status(500).json({error: err});
+        res.status(500).json({
+            error: err
+        });
     })
 });
 module.exports = router;
